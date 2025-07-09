ba1c300677aab8599e5ee599c81f4bef3179c9a1
Stiwo82 <s.schwiertz@gmx.de>
2025-07-09 15:45:04 +0200

    1

---
+++ data.js
@@ -0,0 +1,334 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bar",
+      "name": "Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.4576770874693317,
+        "pitch": 0.1083179481635117,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5452947219478954,
+          "pitch": 0.1540010945375414,
+          "rotation": 0,
+          "target": "1-maske"
+        },
+        {
+          "yaw": 1.9647269978597688,
+          "pitch": 0.06603734989002241,
+          "rotation": 0,
+          "target": "2-gardrobe"
+        },
+        {
+          "yaw": 2.8161731201591724,
+          "pitch": 0.06842770125155795,
+          "rotation": 0,
+          "target": "3-set-1"
+        },
+        {
+          "yaw": -2.702764710695412,
+          "pitch": 0.12916127521654808,
+          "rotation": 0,
+          "target": "4-set-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5051926043271386,
+          "pitch": 0.4961700861987737,
+          "title": "Willkommen",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6566450367132024,
+          "pitch": 0.15205738234324961,
+          "title": "Toiletten",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-maske",
+      "name": "Maske",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.0167114650076847,
+        "pitch": 0.2057362399633771,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0370356533287017,
+          "pitch": 0.11813856406772771,
+          "rotation": 0,
+          "target": "0-bar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1575648536819028,
+          "pitch": 0.021739630898355955,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-gardrobe",
+      "name": "Gardrobe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.492021434814351,
+        "pitch": 0.3294178385886539,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.715481778223971,
+          "pitch": 0.01780057863813944,
+          "rotation": 0,
+          "target": "3-set-1"
+        },
+        {
+          "yaw": -1.1781311408827477,
+          "pitch": 0.10259678871809896,
+          "rotation": 0,
+          "target": "0-bar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17609281288312673,
+          "pitch": 0.3231263010095482,
+          "title": "Kleider",
+          "text": "viele verschiedene"
+        },
+        {
+          "yaw": 2.694802758690477,
+          "pitch": 0.15070198835374882,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.612808397459343,
+          "pitch": 0.1206189186744222,
+          "title": "Kleider",
+          "text": "noch mehr Kleider"
+        }
+      ]
+    },
+    {
+      "id": "3-set-1",
+      "name": "Set 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.7758781856592822,
+        "pitch": 0.32125868616149056,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2394152361582833,
+          "pitch": 0.034617874064764464,
+          "rotation": 0,
+          "target": "2-gardrobe"
+        },
+        {
+          "yaw": 2.2076468941938376,
+          "pitch": 0.04007408760238462,
+          "rotation": 0,
+          "target": "4-set-2"
+        },
+        {
+          "yaw": -3.1174407081357476,
+          "pitch": 0.03476373586630288,
+          "rotation": 0,
+          "target": "0-bar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.250290409610468,
+          "pitch": 0.7581704993411318,
+          "title": "Hängetuch",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.2019814409404361,
+          "pitch": 0.3190455928506104,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-set-2",
+      "name": "Set 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5997585975034951,
+        "pitch": 0.2313062540362676,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.678421819938743,
+          "pitch": 0.15838939980592492,
+          "rotation": 0,
+          "target": "3-set-1"
+        },
+        {
+          "yaw": 2.497272743702627,
+          "pitch": 0.06415351853188689,
+          "rotation": 0,
+          "target": "1-maske"
+        },
+        {
+          "yaw": 1.8139062160587036,
+          "pitch": 0.0791441591046933,
+          "rotation": 0,
+          "target": "0-bar"
+        },
+        {
+          "yaw": 2.9986229632669508,
+          "pitch": 0.060806598236116116,
+          "rotation": 0,
+          "target": "2-gardrobe"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9031583785903621,
+          "pitch": 0.18164926839965645,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "SMF Studio",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
